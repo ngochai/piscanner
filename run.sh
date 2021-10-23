6dc2e9279c6ce24d8e402ca46fb7613e5ad2b15d
Ngoc-Hai Pham <pngochai@yahoo.com>
2021-10-24 00:25:18 +0100

    run

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ gphoto2 --set-config capturetarget=1
 
 #curl "http://192.168.1.28:5000/m2?cw=1&step=200"
 
-for i in {1..20}; do
+for i in {1..22}; do
     for k in {1..20}; do
         curl "http://192.168.1.28:5000/m1?cw=0&step=10"
         sleep 1
         gphoto2 --filename '/mnt/myvault/tmp/images/%Y%m%d%H%M%S%f.%C' --capture-image-and-download
         
     done
-    curl "http://192.168.1.28:5000/m2?cw=1&step=20"
+    curl "http://192.168.1.28:5000/m2?cw=1&step=15"
     sleep 2
 done
